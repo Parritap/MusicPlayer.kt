a5295fe6ca076028e77d34b0b33e91e59b176ee5
Juan Esteban Parra Parra <juane.parrap@uqvirtual.edu.co>
2023-04-08 18:08:38 -0500

    commit privisional

--- src/controllers/TileController.java
+++ src/controllers/TileController.java
@@ -1,4 +1,25 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import model.logic.data.Song;
+
+
 public class TileController {
+
+    @FXML
+    private Text details;
+
+    @FXML
+    private ImageView img;
+
+    @FXML
+    private Label title;
+
+    public void setData (Song song){
+        title.setText(song.getTitle());
+        ImageView.
+    }
 }
