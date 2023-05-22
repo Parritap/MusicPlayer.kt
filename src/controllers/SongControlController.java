883fed93add038bd59e6b185c0966244d1b92362
Juan Esteban Castaño Osma <osmajuan760@gmail.com>
2023-05-22 10:38:09 -0500

    todo funcionando, iniciada funcionalidad para el binding en el control de canciones

--- src/controllers/SongControlController.java
+++ src/controllers/SongControlController.java
@@ -1,14 +1,18 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.ImageView;
 import model.logic.SongsPlayer;
 import model.logic.data.Song;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class SongControlController {
+
+public class SongControlController implements Initializable {
 
     @FXML
     private ImageView favoriteImageView;
@@ -49,7 +53,10 @@ public class SongControlController {
     public void playSong(Song song) {
 
         setearDatos(song);
-        SongsPlayer.playSong(song);
+
+        // not implemented yet, function in class "HorizontalSongController"
+        //SongsPlayer.playSong(song);
+
         cambiarEstadoReproduciendo();
 
     }
@@ -58,7 +65,23 @@ public class SongControlController {
     }
 
     private void setearDatos(Song song) {
-        
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        //test
+        songProgressSlider.valueProperty().bindBidirectional(SongsPlayer.duration);
+
+        // text
+        songNameLbl.textProperty().bindBidirectional(SongsPlayer.name);
+
+        stateSongImageView.setOnMouseClicked(mouseEvent -> {
+            System.out.println("hola mundo");
+            SongsPlayer.testMethod();
+            SongsPlayer.secondTestMethod();
+        });
+
     }
 }
 
--- src/model/logic/SongsPlayer.java
+++ src/model/logic/SongsPlayer.java
@@ -1,17 +1,27 @@
 package model.logic;
 
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
 import model.logic.data.Song;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
 import java.io.File;
+import java.util.Optional;
 
 public class SongsPlayer {
 
+    public static Song currentSong;
+
+    //test
+    public static SimpleStringProperty name = new SimpleStringProperty();
+    public static SimpleDoubleProperty duration = new SimpleDoubleProperty();
+
     public static void playSong(Song song) {
 
+        currentSong = song;
         Clip audioClip = Singleton.getInstance().getAudioClip();
 
         if (song != null) {
@@ -31,7 +41,33 @@ public class SongsPlayer {
 
             Singleton.getInstance().setAudioCLip(audioClip);
         }
-
     }
+
+    public static void pauseCurrentSong() {
+
+        Optional.of(currentSong).ifPresent(song -> {
+            Clip audioClip = Singleton.getInstance().getAudioClip();
+            Optional.of(audioClip).ifPresent((clip -> {
+                clip.stop();
+            }));
+        });
+    }
+
+    public static void continueCurrentSong() {
+
+        Optional.of(currentSong).ifPresent(song -> {
+            Clip audioClip = Singleton.getInstance().getAudioClip();
+            Optional.of(audioClip).ifPresent((clip -> {
+                clip.start();
+            }));
+        });
+    }
+
+    //test
+    public static void testMethod(){
+        name.setValue("hola mundo");
+    }
+
+    public static void secondTestMethod() { duration.setValue(10); }
 
 }
