e8d5f651c945e3dfea1133e4b3c40a5d3a6ab2f5
Juan Esteban Parra Parra <juane.parrap@uqvirtual.edu.co>
2023-05-30 14:47:19 -0500

    bug fixed

--- src/model/logic/SongsExtensions.java
+++ src/model/logic/SongsExtensions.java
@@ -1,7 +1,7 @@
 package model.logic;
 
 public enum SongsExtensions {
-    FLAC("flac"), MP3("mp3"), M4A("m4a"), MP4("mp4"), WAV("wav");
+    FLAC("flac"), MP3("mp3"), M4A("m4a"), MP4("mp4");
     public static final SongsExtensions[] listOfExtensions = SongsExtensions.values();
     private final String extension;
 
